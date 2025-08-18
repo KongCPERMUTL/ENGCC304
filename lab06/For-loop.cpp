67a13664b214ba808aaee715d58b9548e38dd1ac
Danuphat Komek <freetosoon@gmail.com>
2025-08-18 23:52:12 +0700

    update

---
+++ lab06/For-loop.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int N ;
+    printf( " Enter value: " ) ;
+    scanf( " %d " , &N ) ;
+
+    if ( N % 2 == 1 ) { // เลขคี่
+        printf( " Series : " , N ) ;
+        for (int i = 1 ; i <= N ; i += 2 ) {
+            printf( " %d " , i ) ;
+        }
+    } else { // เลขคู่
+        printf( " Series : " , N ) ;
+        for ( int i = N ; i >= 0 ; i -= 2 ) {
+            printf( " %d " , i ) ;
+        }
+    }// end if
+    
+    return 0 ;
+}// end function
