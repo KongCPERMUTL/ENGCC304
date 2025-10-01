b6860b4d325996cfafe3e3e24d10b5d76d81b2d2
Danuphat Komek <freetosoon@gmail.com>
2025-10-01 22:40:47 +0700

    update

---
+++ lab03/lab301.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float H , W , area ;
+
+    printf( "Enter Height (H): " ) ;
+    scanf( "%f" , &H ) ;
+
+    printf( "Enter Width/Base (W): " ) ;
+    scanf( "%f", &W ) ;
+
+    area = ( H * W ) / 2.0 ;
+
+    printf( "Area = %.1f\n", area ) ;
+
+    return 0 ;
+}
